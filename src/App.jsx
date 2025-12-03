3928226f66ec1893d7ee4033a4e1c536bc2cea3b
unknown <sameeksha.tantak@cumminscollege.in>
2025-12-03 21:08:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
