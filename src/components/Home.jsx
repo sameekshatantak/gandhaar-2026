e6af0705e2c6fffb18824dea693401eebafa5b2e
unknown <sameeksha.tantak@cumminscollege.in>
2025-12-24 17:21:17 +0530

    homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,15 @@
 import "../style/Home.css";
-import AboutUs from "./AboutUs";
-import Event from "./Event";
+
 import Footer from "./Footer";
-import GandhaarTheme from "./GandhaarTheme";
+
 import Header from "./Header";
-import ImageGallery from "./ImageGallery";
-import Schedule from "./Schedule";
-import StarLineUp from "./StarLineUp";
+
 
 function Home() {
   return (
     <div>
       <Header/>
-      <GandhaarTheme/>
-      <Event/>
-      <StarLineUp/>
-      <Schedule/>
-      <AboutUs/>
-      <ImageGallery/>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+     
       <Footer/>
     </div>
   );
